71e775aa80782335b45bc1e7da110c538554d92a
Brent Hazman <bdhazman@gmail.com>
2019-02-23 19:58:21 -0500

    Updates

---
+++ precache-manifest.2f8b3109179fb93ff9caef808c4f120f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d56145254a2b4a5186a8",
+    "url": "/colorclock/static/css/main.e96b5de7.chunk.css"
+  },
+  {
+    "revision": "d56145254a2b4a5186a8",
+    "url": "/colorclock/static/js/main.d5614525.chunk.js"
+  },
+  {
+    "revision": "2dd13109ec97fe7ecd04",
+    "url": "/colorclock/static/js/runtime~main.2dd13109.js"
+  },
+  {
+    "revision": "e2a032349bc2797c4db5",
+    "url": "/colorclock/static/js/2.e2a03234.chunk.js"
+  },
+  {
+    "revision": "0afc49f2d50b5890c9b7b82d45d889f3",
+    "url": "/colorclock/static/media/play.0afc49f2.svg"
+  },
+  {
+    "revision": "cd7da8761a8b342ce77eca28d43559c7",
+    "url": "/colorclock/static/media/pause.cd7da876.svg"
+  },
+  {
+    "revision": "568289f950944daa9a9cc26f52b09dff",
+    "url": "/colorclock/index.html"
+  }
+];
